dca53c253252c8d763ab4abac19775c2aa864cd8
Adrian Cooney <cooney.adrian@gmail.com>
2017-02-27 00:10:52 +0000

    Removing useless files.

--- src/util/iterate.js
+++
@@ -1,7 +0,0 @@
-export default function iterate(condition, callback) {
-    return Promise.try(() => {
-        if(condition()) {
-            return callback().then(whileIterate.bind(condition, callback));
-        }
-    });
-}
